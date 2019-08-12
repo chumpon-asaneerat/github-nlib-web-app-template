6d97bc19829db4cf95424ffd5f4bac29c3de0e2e
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-12 23:02:54 +0700

    Update server.js and WebServer class - Add routes.js files for test routes. - Update sendJson method. - Update index.html to call postJson

---
+++ routes/api/public/routes.js
@@ -0,0 +1,61 @@
+//#region common requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+
+// for production
+const nlibPath = path.join(rootPath, 'nlib');
+// for nlib-server dev project
+//const nlibPath = path.join(rootPath, 'src', 'server', 'js', 'nlib');
+
+//const nlibjs = path.join(nlibPath, 'nlib');
+const nlibExprjs = path.join(nlibPath, 'nlib-express');
+
+const WebServer = require(nlibExprjs);
+
+//#endregion
+
+//#region router type and variables
+
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    /**
+     * getJson
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     * @param {WebServer.RequestHandler} next The RequestHandler.
+     */
+    static getJson(req, res, next) {
+        let data = {
+            name: 'Joe',
+            value: Date.now()
+        }
+        WebServer.sendJson(req, res, data);
+    }
+    /**
+     * postJson
+     * @param {Request} req The Request.
+     * @param {Response} res The Response.
+     * @param {WebServer.RequestHandler} next The RequestHandler.
+     */
+    static postJson(req, res, next) {
+        let data = {
+            greating: 'Welcome ' + req.body.name,
+            value: Date.now()
+        }
+        WebServer.sendJson(req, res, data);
+    }
+}
+
+router.get('/getJson', routes.getJson)
+router.post('/postJson', routes.postJson)
+
+const init_routes = (svr) => {
+    svr.route('/api', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
--- server.js
+++ server.js
@@ -4,28 +4,4 @@ const WebServer = require('./nlib/nlib-express');
 
 const wsvr = new WebServer();
 
-const routes = {
-    /** @type {WebServer.RequestHandler} */
-    home: (req, res, next) => {
-        WebServer.sendFile(req, res, 'index.html')
-    },
-    /** @type {WebServer.RequestHandler} */
-    getJson: (req, res, next) => {
-        let data = {
-            name: 'joe',
-            value: Date.now()
-        }
-        WebServer.sendJson(req, res, data);
-    },
-    /** @type {WebServer.RequestHandler} */
-    postJson: (req, res, next) => {
-        let data = { result: 'success' }
-        WebServer.sendJson(req, res, data);
-    }
-}
-
-wsvr.get('/', routes.home);
-wsvr.get('/getJson', routes.getJson);
-wsvr.post('/postJson', routes.postJson);
-
 wsvr.listen();
